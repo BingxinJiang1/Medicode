799a0f6840259ea3c46ec3aff280e0aa6d678ce6
UBC Student <yzyzoe@students.cs.ubc.ca>
2024-04-27 10:49:40 -0700

    p1,2-edit

--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -22,78 +22,73 @@ class IntroScreen extends StatelessWidget {
         elevation: 0,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 100.0, vertical: 20),
-              child: Image.asset('lib/images/heart.jpeg'),
-            ),
-            Expanded(
-              // Wrap with Expanded for proper spacing
-              child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Centers vertically
-                children: [
-                  Text(
-                    'Understand your report',
-                    textAlign:
-                        TextAlign.center, // Centers the text horizontally
-                    style: GoogleFonts.notoSerif(
-                        fontSize: 20, fontWeight: FontWeight.bold),
+        child: SingleChildScrollView(
+          // Use SingleChildScrollView to avoid overflow and allow scrolling
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(100.0, 20.0, 100.0, 20),
+                child: Image.asset('lib/images/heart.jpeg'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(28.0, 0, 28.0, 20.0),
+                child: Text(
+                  'Understand your report',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.notoSerif(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 28.0), // Horizontal padding
+                child: Column(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Aligns text to the left
+                  children: [
+                    Text(
+                      '✓ Upload PDFs of your radiologist\'s reports for instant rephrasing.',
+                      style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                    ),
+                    Text(
+                      '✓ Simplify medical reports into clear, understandable language.',
+                      style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                    ),
+                    Text(
+                      '✓ Get actionable insights and recommended questions for your doctor.',
+                      style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20), // Space before the button
+              Center(
+                // Center the button horizontally
+                child: GestureDetector(
+                  onTap: () => Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => DisclaimerPage()),
                   ),
-                  const SizedBox(
-                      height: 20), // Add space between text and bullet points
-                  Padding(
-                    // Add Padding widget
+                  child: Container(
                     padding: const EdgeInsets.symmetric(
-                        horizontal: 28.0), // Horizontal padding
-                    child: Column(
-                      crossAxisAlignment:
-                          CrossAxisAlignment.start, // Aligns text to the left
-                      children: [
-                        Text(
-                          '• Upload PDFs of your radiologist\'s reports for instant rephrasing.',
-                          style:
-                              TextStyle(fontSize: 20, color: Colors.grey[700]),
-                        ),
-                        Text(
-                          '• Simplify medical reports into clear, understandable language.',
-                          style:
-                              TextStyle(fontSize: 20, color: Colors.grey[700]),
-                        ),
-                        Text(
-                          '• Get actionable insights and recommended questions for your doctor.',
-                          style:
-                              TextStyle(fontSize: 20, color: Colors.grey[700]),
-                        ),
-                      ],
+                        horizontal: 24,
+                        vertical: 12), // Adjust padding as needed
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: mint,
+                    ),
+                    child: const Text(
+                      'Get Started',
+                      style: TextStyle(color: Colors.black, fontSize: 16),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Center( // Center the button horizontally
-              child: GestureDetector(
-                onTap: () => Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => DisclaimerPage()),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12), // Adjust padding as needed
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: mint,
-                  ),
-                  child: const Text(
-                    'Get Started',
-                    style: TextStyle(color: Colors.black, fontSize: 16),
-                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30), // To ensure some space at the bottom of the screen
-          ]
+              const SizedBox(
+                  height:
+                      30), // To ensure some space at the bottom of the screen
+            ],
+          ),
         ),
       ),
     );
