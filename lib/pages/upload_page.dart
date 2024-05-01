68b093bd166b2e3f4533fb3f5906b75ac9deda24
Charity <charity.grey1@gmail.com>
2024-04-30 22:42:32 -0700

    Update Files for Charity to run emulator

--- lib/pages/upload_page.dart
+++ lib/pages/upload_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:gemini/constants.dart';
+import 'package:gemini/components/constants.dart';
 import 'package:gemini/pages/feedback.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:gemini/pages/disclaimer_screen.dart';
