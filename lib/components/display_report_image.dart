73c0fc17541488173012a1d7cb1cad0fbd27f673
Charity <charity.grey1@gmail.com>
2024-05-01 21:54:54 -0700

    Added async geminiAnalyze function

--- lib/components/display_report_image.dart
+++ lib/components/display_report_image.dart
@@ -19,30 +19,34 @@ class displayReportImage extends StatefulWidget  {
 
 class displayReportImageState extends State<displayReportImage> {
   final Color mint = Color.fromARGB(255, 162, 228, 184);
-  // void geminiAnalyze() async {
-  //   final apiKey = 'AIzaSyDc8aYbZAgj1ZH5zKUUgD7y7JfZNYpNkpI';
-  //   final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey!);
-  //   final prompt = 'You are an image-to-text converter. Please take this image and convert it to text, exactly as in the photo.';
-  //   try {
-  //     final Uint8List raw_file = await supabase
-  //       .storage
-  //       .from('report_images')
-  //       .download(widget.fileUrl);
+
+  void geminiAnalyze() async {
+    final apiKey = 'AIzaSyDc8aYbZAgj1ZH5zKUUgD7y7JfZNYpNkpI';
+    final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey!);
+    final prompt = 'You are an image-to-text converter. Please take this image and convert it to text, exactly as in the photo.';
+
+    try {
+      final Uint8List raw_file = await supabase
+        .storage
+        .from('report_images')
+        .download(widget.fileUrl);
       
-  //     final imageBytes = await File.fromRawPath(raw_file).readAsBytes();
-  //     final convertedText = [
-  //        Content.multi([
-  //         TextPart(prompt),
-  //         DataPart('image/png', imageBytes),
-  //     ])];
+      final imageBytes = await File.fromRawPath(raw_file).readAsBytes();
+      final convertedText = [
+         Content.multi([
+          TextPart(prompt),
+          DataPart('image/png', imageBytes),
+      ])];
       
-  //     final response = await model.generateContent(convertedText);
-  //     print(response.text);
-  //   } catch (error)  {
-  //     //TODO
-  //   }
-  //  }
-  
+      final response = await model.generateContent(convertedText);
+      print(response.text);
+    } catch (error)  {
+      ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Failed to convert image into text: $error'))
+      );
+    }
+  }
+    
     @override
     Widget build(BuildContext context) {
       return MaterialButton(
